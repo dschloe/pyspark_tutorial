44c9ab08d54e8bbdd224dd357e8243c7de52267e
dschloe <j2hoon85@gmail.com>
2022-01-12 17:50:16 +0900

    updated

--- section_01_get_started/ratings-counter.py
+++ section_01_get_started/ratings-counter.py
@@ -1,5 +1,12 @@
+# SparkContext:
+# Spark framework to create RDD
+# Can't Create SparkContext without SparkConf
+# Configure
 from pyspark import SparkConf, SparkContext
 import collections
+
+# MasterNode = local
+# It's using Elastic MapReduce
 
 conf = SparkConf().setMaster("local").setAppName("RatingsHistogram")
 sc = SparkContext(conf = conf)
@@ -7,7 +14,9 @@ sc = SparkContext(conf = conf)
 # lines = sc.textFile("file:///SparkCourse/ml-100k/u.data")
 lines = sc.textFile("ml-100k/u.data")
 ratings = lines.map(lambda x: x.split()[2])
+print("ratings: ", ratings)
 result = ratings.countByValue()
+print("result: ", result)
 
 sortedResults = collections.OrderedDict(sorted(result.items()))
 for key, value in sortedResults.items():
